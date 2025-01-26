242673d2f86417c6eba51ed711d889f5c1242b15
Dawit212119 <dawit.workye-ug@aau.edu.et>
2025-01-26 21:17:20 +0300

    update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,141 +1,137 @@
-// import Header from "../components/Header/Header";
-// import Startup from "../components/Header/StartupLogo/Startup";
-// import MyName from "../components/Home/MyName/MyName";
-// import { useContext, useEffect, useState, useRef } from "react";
-// import SocialMediaArround from "../components/Home/SocialMediaArround/SocialMediaArround";
-// import AboutMe from "../components/Home/AboutMe/AboutMe";
-// import ThisCantBeReached from "../components/Home/ThisSiteCantBeReached/ThisCantBeReached";
-// import WhereIHaveWorked from "../components/Home/WhereIHaveWorked/WhereIHaveWorked";
-// import SomethingIveBuilt from "../components/Home/SomethingIveBuilt/SomethingIveBuilt";
-// import GetInTouch from "../components/Home/GetInTouch/GetInTouch";
-// import Footer from "../components/Footer/Footer";
-// import AppContext from "../components/AppContextFolder/AppContext";
-// import Aos from "aos";
-// import "aos/dist/aos.css";
-// import Head from "next/head";
-// import ScreenSizeDetector from "../components/CustomComponents/ScreenSizeDetector";
+import Header from "../components/Header/Header";
+import Startup from "../components/Header/StartupLogo/Startup";
+import MyName from "../components/Home/MyName/MyName";
+import { useContext, useEffect, useState, useRef } from "react";
+import SocialMediaArround from "../components/Home/SocialMediaArround/SocialMediaArround";
+import AboutMe from "../components/Home/AboutMe/AboutMe";
+import ThisCantBeReached from "../components/Home/ThisSiteCantBeReached/ThisCantBeReached";
+import WhereIHaveWorked from "../components/Home/WhereIHaveWorked/WhereIHaveWorked";
+import SomethingIveBuilt from "../components/Home/SomethingIveBuilt/SomethingIveBuilt";
+import GetInTouch from "../components/Home/GetInTouch/GetInTouch";
+import Footer from "../components/Footer/Footer";
+import AppContext from "../components/AppContextFolder/AppContext";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import Head from "next/head";
+import ScreenSizeDetector from "../components/CustomComponents/ScreenSizeDetector";
 
-// export default function Home() {
-//   const [ShowElement, setShowElement] = useState(false);
-//   const [ShowThisCantBeReached, setShowThisCantBeReached] = useState(true);
-//   const [ShowMe, setShowMe] = useState(false);
+export default function Home() {
+  const [ShowElement, setShowElement] = useState(false);
+  const [ShowThisCantBeReached, setShowThisCantBeReached] = useState(true);
+  const [ShowMe, setShowMe] = useState(false);
 
-//   const context = useContext(AppContext);
-//   const aboutRef = useRef<HTMLDivElement>(null);
-//   const homeRef = useRef<HTMLDivElement>(null);
+  const context = useContext(AppContext);
+  const aboutRef = useRef<HTMLDivElement>(null);
+  const homeRef = useRef<HTMLDivElement>(null);
 
-//   useEffect(() => {
-//     clearInterval(context.sharedState.userdata.timerCookieRef.current);
-//     if (typeof window !== "undefined") {
-//       window.removeEventListener(
-//         "resize",
-//         context.sharedState.userdata.windowSizeTracker.current
-//       );
-//       window.removeEventListener(
-//         "mousemove",
-//         context.sharedState.userdata.mousePositionTracker.current,
-//         false
-//       );
-//       window.removeEventListener(
-//         "resize",
-//         context.sharedState.typing.eventInputLostFocus
-//       );
-//       document.removeEventListener(
-//         "keydown",
-//         context.sharedState.typing.keyboardEvent
-//       );
-//     }
-//     setTimeout(() => {
-//       setShowElement(true);
-//     }, 4500);
+  useEffect(() => {
+    clearInterval(context.sharedState.userdata.timerCookieRef.current);
+    if (typeof window !== "undefined") {
+      window.removeEventListener(
+        "resize",
+        context.sharedState.userdata.windowSizeTracker.current
+      );
+      window.removeEventListener(
+        "mousemove",
+        context.sharedState.userdata.mousePositionTracker.current,
+        false
+      );
+      window.removeEventListener(
+        "resize",
+        context.sharedState.typing.eventInputLostFocus
+      );
+      document.removeEventListener(
+        "keydown",
+        context.sharedState.typing.keyboardEvent
+      );
+    }
+    setTimeout(() => {
+      setShowElement(true);
+    }, 4500);
 
-//     setTimeout(() => {
-//       setShowThisCantBeReached(false);
-//     }, 5400);
+    setTimeout(() => {
+      setShowThisCantBeReached(false);
+    }, 5400);
 
-//     setTimeout(() => {
-//       setShowElement(false);
-//       setShowMe(true);
-//       context.sharedState.finishedLoading = true;
-//       context.setSharedState(context.sharedState);
-//     }, 10400);
-//   }, [context, context.sharedState]);
+    setTimeout(() => {
+      setShowElement(false);
+      setShowMe(true);
+      context.sharedState.finishedLoading = true;
+      context.setSharedState(context.sharedState);
+    }, 10400);
+  }, [context, context.sharedState]);
 
-//   useEffect(() => {
-//     Aos.init({ duration: 2000, once: true });
-//   }, []);
+  useEffect(() => {
+    Aos.init({ duration: 2000, once: true });
+  }, []);
 
-//   console.log("website is rendering...");
+  console.log("website is rendering...");
 
-//   const meta = {
-//     title: "Abdellatif Anaflous - Software Engineer",
-//     description: `I've been working on Software development for 5 years straight. Get in touch with me to know more.`,
-//     image: "/titofCercle.png",
-//     type: "website",
-//   };
-//   const isProd = process.env.NODE_ENV === "production";
+  const meta = {
+    title: "Dawit Workye - Software Engineer",
 
-//   return (
-//     <>
-//       {/* <Head>
-//         {/* <title>{meta.title}</title>
-//         <meta name="robots" content="follow, index" />
-//         <meta content={meta.description} name="description" />
-//         <meta property="og:url" content={`https://anaflous.com`} />
-//         <link rel="canonical" href={``} />
-//         <meta property="og:type" content={meta.type} />
-//         <meta property="og:site_name" content="" />
-//         <meta property="og:description" content={meta.description} />
-//         <meta property="og:title" content={meta.title} />
-//         <meta property="og:image" content={meta.image} />
-//         <meta name="twitter:card" content="summary_large_image" />
-//         <meta name="twitter:site" content="@titofabdo" />
-//         <meta name="twitter:title" content={meta.title} />
-//         <meta name="twitter:description" content={meta.description} />
-//         <meta name="twitter:image" content={meta.image} /> */}
-//       </Head> */}
+    image: "port.png",
+    type: "website",
+  };
+  const isProd = process.env.NODE_ENV === "production";
 
-//       <div className="relative snap-mandatory min-h-screen bg-AAprimary w-full ">
-//         {context.sharedState.finishedLoading ? (
-//           <></>
-//         ) : ShowThisCantBeReached ? (
-//           <ThisCantBeReached />
-//         ) : (
-//           <></>
-//         )}
-//         {context.sharedState.finishedLoading ? (
-//           <></>
-//         ) : ShowElement ? (
-//           <Startup />
-//         ) : (
-//           <></>
-//         )}
-//         <Header
-//           finishedLoading={context.sharedState.finishedLoading}
-//           sectionsRef={homeRef}
-//         />
-//         <MyName finishedLoading={context.sharedState.finishedLoading} />
-//         <SocialMediaArround
-//           finishedLoading={context.sharedState.finishedLoading}
-//         />
-//         {context.sharedState.finishedLoading ? (
-//           <AboutMe ref={aboutRef} />
-//         ) : (
-//           <></>
-//         )}
-//         {context.sharedState.finishedLoading ? <WhereIHaveWorked /> : <></>}
-//         {context.sharedState.finishedLoading ? <SomethingIveBuilt /> : <></>}
-//         {context.sharedState.finishedLoading ? <GetInTouch /> : <></>}
-//         {context.sharedState.finishedLoading ? (
-//           <Footer
-//             githubUrl={"https://github.com/hktitof/my-website"}
-//             hideSocialsInDesktop={true}
-//           />
-//         ) : (
-//           <></>
-//         )}
-//         {!isProd && <ScreenSizeDetector />}
-//       </div>
-//     </>
-//   );
-// }
+  return (
+    <>
+      <Head>
+        <title>{meta.title}</title>
+        <meta name="robots" content="follow, index" />
+        <meta property="og:url" content={`https://anaflous.com`} />
+        <link rel="canonical" href={``} />
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="" />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:image" content={meta.image} />
+      </Head>
+
+      <div className="relative snap-mandatory min-h-screen bg-AAprimary w-full ">
+        {context.sharedState.finishedLoading ? (
+          <></>
+        ) : ShowThisCantBeReached ? (
+          <ThisCantBeReached />
+        ) : (
+          <></>
+        )}
+        {context.sharedState.finishedLoading ? (
+          <></>
+        ) : ShowElement ? (
+          <Startup />
+        ) : (
+          <></>
+        )}
+        <Header
+          finishedLoading={context.sharedState.finishedLoading}
+          sectionsRef={homeRef}
+        />
+        <MyName finishedLoading={context.sharedState.finishedLoading} />
+        <SocialMediaArround
+          finishedLoading={context.sharedState.finishedLoading}
+        />
+        {context.sharedState.finishedLoading ? (
+          <AboutMe ref={aboutRef} />
+        ) : (
+          <></>
+        )}
+        {context.sharedState.finishedLoading ? <WhereIHaveWorked /> : <></>}
+        {context.sharedState.finishedLoading ? <SomethingIveBuilt /> : <></>}
+        {context.sharedState.finishedLoading ? <GetInTouch /> : <></>}
+        {context.sharedState.finishedLoading ? (
+          <Footer
+            githubUrl={"https://github.com/hktitof/my-website"}
+            hideSocialsInDesktop={true}
+          />
+        ) : (
+          <></>
+        )}
+        {!isProd && <ScreenSizeDetector />}
+      </div>
+    </>
+  );
+}
