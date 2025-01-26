46d6bd1587450f9a00ef46af6c81360eec883ef0
Dawit212119 <dawit.workye-ug@aau.edu.et>
2025-01-26 20:02:59 +0300

    update Logo

--- components/Header/Headercomp/Logo.tsx
+++ components/Header/Headercomp/Logo.tsx
@@ -4,19 +4,19 @@ import { motion } from "framer-motion";
 export default function Logo(props: { finishedLoading: boolean }) {
   return (
     <motion.div
-      initial={{ y: 0, opacity: 0 }}
+      initial={{ y: props.finishedLoading ? 0 : 8, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{
         type: "spring",
-        y: { delay: props.finishedLoading ? 0 : 8, duration: 0 },
-        opacity: { delay: props.finishedLoading ? 0 : 8, duration: 0 },
+        delay: props.finishedLoading ? 0 : 8,
+        duration: 0,
       }}
       className="relative h-12 w-10"
     >
       {/* Red "D" */}
       <motion.span
         initial={{ x: 1 }}
-        className="absolute h-full w-full flex justify-center items-center text-lg font-Header text-[#ff0000]"
+        className="absolute h-full w-full flex justify-center items-center text-lg font-Header text-green-500"
       >
         D
       </motion.span>
@@ -24,27 +24,27 @@ export default function Logo(props: { finishedLoading: boolean }) {
       {/* Decorative lines in red */}
       <motion.div
         animate={{ rotate: -30, y: 5 }}
-        className="h-1 w-6 rounded bg-[#ff0000]" // Set background color to red
+        className="h-1 w-6 rounded text-green-500"
       ></motion.div>
       <motion.div
         animate={{ rotate: 90, x: -10, y: 18 }}
-        className="h-1 w-6 rounded bg-[#ff0000]" // Set background color to red
+        className="h-1 w-6 rounded text-green-500"
       ></motion.div>
       <motion.div
         animate={{ rotate: 30, y: 31 }}
-        className="h-1 w-6 rounded bg-[#ff0000]" // Set background color to red
+        className="h-1 w-6 rounded text-green-500"
       ></motion.div>
       <motion.div
         animate={{ rotate: -30, y: 27, x: 19 }}
-        className="h-1 w-6 rounded bg-[#ff0000]" // Set background color to red
+        className="h-1 w-6 rounded text-green-500"
       ></motion.div>
       <motion.div
         animate={{ rotate: 30, x: 19, y: -10 }}
-        className="h-1 w-6 rounded bg-[#ff0000]" // Set background color to red
+        className="h-1 w-6 rounded text-green-500"
       ></motion.div>
       <motion.div
         animate={{ rotate: 90, x: 28, y: 2 }}
-        className="h-1 w-6 rounded bg-[#ff0000]" // Set background color to red
+        className="h-1 w-6 rounded text-green-500"
       ></motion.div>
     </motion.div>
   );
